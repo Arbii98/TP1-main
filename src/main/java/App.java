197dc85a6753add66b1a4160a13c5872e23ccbd9
Arbii98 <arbisaidi8@gmail.com>
2021-11-19 17:04:39 +0100

    Last commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,16 @@
+import org.apache.log4j.*;
+
 public class App {
 
 	private static Zoo zoo;
+	private static final Logger logger = LogManager.getLogger(App.class);
+	
+
 	
 	public static void main(String[] args) {
 		zoo = new Zoo();
 		System.out.println("Test");
+		logger.info("Application run successfully");
 	}
 	
 	public static Zoo getZoo() {
